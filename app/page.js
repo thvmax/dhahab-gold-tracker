9aed0e5f76566791b0648cff86b86ee789f1ea88
thvmax <thutasoe24@gmail.com>
2026-04-19 23:55:44 +0400

    Modified User Client

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+"use client";
 
 import { useState, useEffect, useRef } from "react";
 import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer, CartesianGrid, ReferenceLine } from "recharts";
